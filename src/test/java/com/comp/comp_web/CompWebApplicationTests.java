d5de9dd879bc1427c1307ac662803f533ddb887b
chamelewan <wani001110@gmail.com>
2026-02-13 17:53:12 +0900

    feat: Initial Spring Boot project setup

---
+++ src/test/java/com/comp/comp_web/CompWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.comp.comp_web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
