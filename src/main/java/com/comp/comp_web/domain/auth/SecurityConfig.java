d12ce7cfa1d0df1700d926b980ce1bb2ae056463
Lidohyeon <ehgus3193@naver.com>
2026-02-28 16:45:11 +0900

    refacotr: config 도메인으로 추가

--- src/main/java/com/comp/comp_web/domain/auth/SecurityConfig.java
+++ src/main/java/com/comp/comp_web/domain/auth/SecurityConfig.java
@@ -1,4 +1,4 @@
-package com.comp.comp_web.config;
+package com.comp.comp_web.domain.auth;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
