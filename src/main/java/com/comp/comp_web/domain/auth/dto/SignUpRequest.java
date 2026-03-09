f86c5f6b7ad1d570adc610d8d4b3c2e835d2c185
Lidohyeon <ehgus3193@naver.com>
2026-03-09 12:26:53 +0900

    refactor: signup 비밀번호 패턴 추가

--- src/main/java/com/comp/comp_web/domain/auth/dto/SignUpRequest.java
+++ src/main/java/com/comp/comp_web/domain/auth/dto/SignUpRequest.java
@@ -1,9 +1,6 @@
 package com.comp.comp_web.domain.auth.dto;
 
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -17,6 +14,10 @@ public class SignUpRequest {
 
     @NotBlank(message = "비밀번호는 필수입니다")
     @Size(min = 8, message = "비밀번호는 최소 8자 이상이어야 합니다")
+    @Pattern(
+        regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$",
+        message = "비밀번호는 대문자, 소문자, 숫자, 특수문자(@$!%*?&)를 각각 최소 1개씩 포함해야 합니다"
+    )
     private String password;
 
     @NotBlank(message = "이름은 필수입니다")
